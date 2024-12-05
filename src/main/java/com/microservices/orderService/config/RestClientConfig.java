a0614af3c3e618daf7c2931a6db8dbe0b7722af3
Sasha <55playn77@gmail.com>
2024-12-05 14:46:31 +0300

    Implementing Observability with Grafana Stack

--- src/main/java/com/microservices/orderService/config/RestClientConfig.java
+++ src/main/java/com/microservices/orderService/config/RestClientConfig.java
@@ -1,6 +1,8 @@
 package com.microservices.orderService.config;
 
 import com.microservices.orderService.client.InventoryClient;
+import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,15 +15,18 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import java.time.Duration;
 
 @Configuration
+@RequiredArgsConstructor
 public class RestClientConfig {
     @Value("${inventory.url}")
     private String inventoryServiceUrl;
+    private final ObservationRegistry observationRegistry;
 
     @Bean
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
                 .requestFactory(getClientRequestFactory())
+                .observationRegistry(observationRegistry)
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
